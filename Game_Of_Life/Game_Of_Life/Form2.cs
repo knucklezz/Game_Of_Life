7cfe2eb7c3e023e2e37f2b5c0336cf6a0e48e8f7
Niklas Lundström <te14.niklas.lundstrom@umea.nti.se>
2019-01-10 14:13:17 +0100

    Added form2 and fixed some methods

---
+++ Game_Of_Life/Game_Of_Life/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Of_Life
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (GameName.Text == "")
+            {
+                MessageBox.Show("You must give your save a name!");
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
