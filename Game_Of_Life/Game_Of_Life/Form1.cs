22580a72a9bec1ac0982c4724882a666579d1a7f
Niklas Lundström <te14.niklas.lundstrom@umea.nti.se>
2019-01-10 13:24:19 +0100

    connected some gamelogic to buttons

--- Game_Of_Life/Game_Of_Life/Form1.cs
+++ Game_Of_Life/Game_Of_Life/Form1.cs
@@ -12,16 +12,65 @@ namespace Game_Of_Life
 {
     public partial class Form1 : Form
     {
+        //private bool gameRunning = false;
+        private GameLogic gameLogicInstance= new GameLogic();
+        private bool[][] currentBoard;
+
+
 
         public Form1()
         {
             InitializeComponent();
+            InitializeTimer();
         }
 
         private void LoadSaveButton_Click(object sender, EventArgs e)
         {
             Form LoadSaveForm = new Form();
             LoadSaveForm.Show();
+        }
+
+        private void GridPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            updateGameBoard(currentBoard);
+        }
+
+        private void randomGameButton_Click(object sender, EventArgs e)
+        {
+           currentBoard = gameLogicInstance.GetNewGame();
+
+        }
+        private void InitializeTimer()
+        {
+            timer.Interval = 500;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            TimerLabel.Text = DateTime.Now.ToString();
+            
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void updateGameBoard(bool[][] currentBoard)
+        {
+
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            currentBoard = gameLogicInstance.GetNextGeneration();
         }
     }
 }
